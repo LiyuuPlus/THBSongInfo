53a8617f45903893adeb71eb81f78c01e8ee61f5
whuihuan <whuihuan@outlook.com>
2023-06-23 13:16:42 +0800

    修正THB炸的时候，歌词获取错误的问题

--- main.js
+++ main.js
@@ -5,9 +5,8 @@ window.onProcessLyrics = async (lyrics, id) => {
     let newLyrics = lyrics;
     try{
         //获得该歌曲的THB信息
-        let lRes = await fetch(`https://thb.liyuu.plus/netease/detail/${id}/thb`);
-        let lInfo = await lRes.json();
-        if(lInfo.code == 200)
+        let lInfo = await httpGet(`https://thb.liyuu.plus/netease/detail/${id}/thb`,"json");
+        if(lInfo && lInfo.code == 200)
         {
             let data = lInfo.data;
             //存入window待使用
@@ -15,13 +14,14 @@ window.onProcessLyrics = async (lyrics, id) => {
             let lyricData = data.lyricInfo;
             if(lyricData)
             {
-                let lyric = await fetch(lyricData.lyricUrl);
-                let lyricInfo = await lyric.text();
+                let lyricInfo = await httpGet(lyricData.lyricUrl);
                 lyricInfo += "[999:999:999]本歌词来自THBWiki";
-                let tlyric = await fetch(lyricData.transLyricUrl);
-                let tlyricInfo = await tlyric.text();
+                let tlyricInfo = await httpGet(lyricData.transLyricUrl);
                 tlyricInfo += "[999:999:999]lyrics.thwiki.cc";
-                newLyrics = { status: 200, lrc: { lyric: lyricInfo, version: 1 }, tlyric: { lyric: tlyricInfo, version: 1 } };
+                if(lyricInfo)
+                {
+                    newLyrics = { status: 200, lrc: { lyric: lyricInfo, version: 1 }, tlyric: { lyric: tlyricInfo, version: 1 } };
+                }
             }
         }
         else{
@@ -32,4 +32,37 @@ window.onProcessLyrics = async (lyrics, id) => {
         newLyrics = lyrics;
     }
     return _onProcessLyrics(newLyrics);
+}
+
+const httpGet = (url,retType = 'text') =>{
+    let error = {
+        status:503,
+        trace:null
+    };
+    return new Promise(async (resolve,reject)=>{
+        let result = await fetch(url).then(async(res)=>{
+            if(res.status == 200)
+            {
+                switch(retType)
+                {
+                    case "json":{
+                        let rJson = await res.json();
+                        return resolve(rJson);
+                    }break;
+                    default:{
+                        let rText = await res.text();
+                        return resolve(rText);
+                    }
+                }
+            }
+            else
+            {
+                error.status == res.status;
+                return reject(error);
+            }
+        }).catch(err=>{
+            error.trace = err;
+            return reject(error);
+        })
+    });
 }
